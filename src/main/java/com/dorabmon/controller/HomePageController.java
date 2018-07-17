95391c81f59cf6c688678bd1807237c9e23aced3
tobyzhong <zhongziyun1993@gmail.com>
2018-07-16 23:00:57 -0300

    add jsp front file

--- src/main/java/com/dorabmon/controller/HomePageController.java
+++ src/main/java/com/dorabmon/controller/HomePageController.java
@@ -1,4 +1,18 @@
 package com.dorabmon.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
 public class HomePageController {
+    @RequestMapping(value = "/home_page", method = RequestMethod.GET)
+
+    public ModelAndView admin (HttpServletRequest httpServletRequest) {
+        return new ModelAndView("home_page");
+    }
+
 }
