31207f1039d5a8f4bba5c662a52f47e3763ce92f
Yunfei <philipguo1992@gmail.com>
2018-07-17 20:17:41 -0300

    court controller

--- src/main/java/com/dorabmon/controller/AdminDashboardController.java
+++ src/main/java/com/dorabmon/controller/AdminDashboardController.java
@@ -2,16 +2,21 @@ package com.dorabmon.controller;
 
 import com.dorabmon.service.user.UserService;
 import com.dorabmon.service.user.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Controller
 public class AdminDashboardController {
-    UserService userService = new UserServiceImpl();
-    @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
 
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
     public ModelAndView admin (HttpServletRequest httpServletRequest) {
         return new ModelAndView("admin");
     }
