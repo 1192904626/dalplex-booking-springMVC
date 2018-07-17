abe229b106bc6951daed82d14c5af74515bfb1ea
Unknown <yiren1105@gmail.com>
2018-07-16 23:17:53 -0300

    login

--- src/main/java/com/dorabmon/service/UserService.java
+++ src/main/java/com/dorabmon/service/UserService.java
@@ -5,5 +5,5 @@ import com.dorabmon.model.User;
 import java.sql.SQLException;
 
 public interface UserService extends BaseService<User>{
-
+    User Login(String email, String password);
 }
--- src/main/java/com/dorabmon/service/UserServiceImpl.java
+++ src/main/java/com/dorabmon/service/UserServiceImpl.java
@@ -4,6 +4,8 @@ import com.dorabmon.dao.UserDao;
 import com.dorabmon.dao.UserDaoImpl;
 import com.dorabmon.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
@@ -15,20 +17,36 @@ public class UserServiceImpl implements UserService{
     @Autowired
     private UserDao userDao;
 
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @Override
     public void Insert(User entity) {
-
+        try {
+            entity.setPassword(passwordEncoder.encode(entity.getPassword()));
+            if (userDao.FindById(entity.getStudent_id()) == null){
+                userDao.Insert(entity);
+            }
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void Update(User entity) {
-
+        try {
+            userDao.Update(entity);
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void Delete(String id) {
-
+        try {
+            userDao.Delete(id);
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -43,6 +61,24 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public List<User> FindAll() {
-        return null;
+        try {
+            return userDao.FindAll();
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public User Login(String email, String password) {
+        try {
+            User user = userDao.FindByEmail(email);
+            if (user != null && passwordEncoder.matches(password, user.getPassword())){
+                return user;
+            }else {
+                return null;
+            }
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
