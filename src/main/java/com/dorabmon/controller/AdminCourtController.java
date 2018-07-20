bd7a2d712fe3d33ea55a4a5fdba29ba023334465
Unknown <yiren1105@gmail.com>
2018-07-20 15:00:25 -0300

    Add jackson dependency Update admin court

--- src/main/java/com/dorabmon/controller/AdminCourtController.java
+++ src/main/java/com/dorabmon/controller/AdminCourtController.java
@@ -4,13 +4,10 @@ import com.dorabmon.model.Court;
 import com.dorabmon.service.court.CourtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -27,6 +24,13 @@ public class AdminCourtController {
         modelAndView.addObject("courtList", courtList);
         return modelAndView;
     }
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public ModelAndView addCourt(@ModelAttribute Court court, HttpServletRequest httpServletRequest){
+        courtService.Insert(court);
+        return courtlist(httpServletRequest);
+    }
+
     @RequestMapping(value = "/{courtid}", method = RequestMethod.GET)
     public ModelAndView courtdelete (@PathVariable("courtid") String courtid, HttpServletRequest httpServletRequest) {
         courtService.Delete(courtid);
@@ -36,8 +40,28 @@ public class AdminCourtController {
     }
 
 
-    
+    @RequestMapping(value = "admin/{courtid}", method = RequestMethod.GET)
+    @ResponseBody
+    public Court getCourt(@PathVariable("courtid") int courtid,
+                          HttpServletRequest httpServletRequest){
+
+        return courtService.FindById(courtid);
+
+    }
 
 
+    @RequestMapping(value = "admin/{courtid}", method = RequestMethod.POST)
+    public ModelAndView updateCourt(@ModelAttribute Court court, HttpServletRequest httpServletRequest){
+        courtService.Update(court);
+        return courtlist(httpServletRequest);
+    }
+
+
+    @RequestMapping(value = "admin/list", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Court> getCourt(HttpServletRequest httpServletRequest){
+
+        return courtService.FindAll();
+    }
 
 }
