76d2ea1f30bec6108b2d0f942818525cbb056cf1
tobyzhong <zhongziyun1993@gmail.com>
2018-07-26 22:08:26 -0300

    update for as3 [zyzhong]

--- src/main/webapp/WEB-INF/pages/js/studentPage.js
+++ src/main/webapp/WEB-INF/pages/js/studentPage.js
@@ -2,3 +2,38 @@
  * Written by Ziyun Zhong
  */
 
+    // pass value to modal
+function deletecourse(element) {
+        var courseid = element.getAttribute('value');
+        $("#deletecourse").val(courseid);
+    };
+$("#deletecourse").click(function(){
+    //get id
+    var courseid = $("#deletecourse").val();
+    $.ajax({
+        url:"/student_page/"+courseid,
+        type: 'GET',
+        success: function (data, status){
+            if(status == "success"){
+                location.reload();
+            };
+        }
+    })
+});
+function insertcourse(element) {
+    var courseid = element.getAttribute('value');
+    $("#insertcourse").val(courseid);
+};
+$("#insertcourse").click(function(){
+    //get id
+    var courseid = $("#insertcourse").val();
+    $.ajax({
+        url:"/student_page/"+courseid,
+        type: 'POST',
+        success: function (data, status){
+            if(status == "success"){
+                location.reload();
+            };
+        }
+    })
+});
