0e8933e8c373cce8fd43e217341994ba1b6c7f09
Unknown <yiren1105@gmail.com>
2018-08-07 20:43:17 -0300

    Auto stash before merge of "dev-course" and "origin/dev-course"
    useless lib

--- src/main/java/com/dorabmon/dao/studentCourse/StudentCourseDaoImpl.java
+++ src/main/java/com/dorabmon/dao/studentCourse/StudentCourseDaoImpl.java
@@ -3,7 +3,6 @@ package com.dorabmon.dao.studentCourse;
 
 import com.dorabmon.model.User;
 import com.dorabmon.util.DBCPUtil;
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
